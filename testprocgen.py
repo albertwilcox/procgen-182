eae1823bd6b21f42fc6e4b146ad40dd87dc41b86
Albert Wilcox <albertwilcoxiii@gmail.com>
2020-04-25 15:01:37 -0700

    added some files

---
+++ testprocgen.py
@@ -0,0 +1,8 @@
+import gym
+env = gym.make('procgen:procgen-coinrun-v0')
+obs = env.reset()
+while True:
+    obs, rew, done, info = env.step(env.action_space.sample())
+    env.render()
+    if done:
+        break
